924864b3f5e2f2a9e6ae808773e97db0abe858c2
Enderzombie <80001985+exo-enrico@users.noreply.github.com>
2025-08-13 14:40:40 +0200

    NPM publish commit

---
+++ src/objects/core/colors.js
@@ -0,0 +1,25 @@
+const colors = {
+    // Die entsprechenden Daten, welche durch die Funktionen gefüllt werden.
+    data: {
+        random: Number
+    },
+    // Paar wichtige Farben.
+    branding: {
+        blue: '#00689e',
+        darkBlue: '#004aad',
+        skyBlue: '#459cc8',
+        babyBlue: '#2faee3',
+        red: '#a60a1a',
+        grey: '#888888',
+        black: '#000000',
+        white: '#ffffff'
+    },
+    // Generiert eine zufällige Farbe.
+    getRandom() {
+        this.data.random = Math.floor(Math.random() * (0xffffff + 1));
+    }
+}
+
+module.exports = {
+    colors
+}
